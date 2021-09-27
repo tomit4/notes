83cfcb57459b01ffdcd0b8f46376644787520d3f
tomit4 <mosssap@gmail.com>
2021-09-27 11:16:00 -0700

    added micro settings

---
+++ micro/plug/nordcolors/nordcolors.lua
@@ -0,0 +1,9 @@
+VERSION = "1.1.4"
+
+local config = import("micro/config")
+
+config.AddRuntimeFile("nordcolors", config.RTColorscheme, "colorschemes/nord-16.micro")
+config.AddRuntimeFile("nordcolors", config.RTColorscheme, "colorschemes/nord-tc.micro")
+config.AddRuntimeFile("nordcolors", config.RTColorscheme, "colorschemes/nord-16-light.micro")
+config.AddRuntimeFile("nordcolors", config.RTColorscheme, "colorschemes/nord-tc-light.micro")
+config.AddRuntimeFile("nordcolors", config.RTHelp, "help/nordcolors.md")
