912164bd9b0676da34c6bcf0d6fac97241dc80ae
tomit4 <mosssap@gmail.com>
2022-05-23 11:37:02 -0700

    updated st settings

--- st/config.h
+++ st/config.h
@@ -95,7 +95,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.6;
+float alpha = 0.7;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
