acbcf8a81f82e16208fbfafd680ed5d2ca8ff60f
tomit4 <mosssap@gmail.com>
2022-06-15 13:44:30 -0700

    added if statement to shutdown.sh and reboot.sh that removes zsh_history on shutdown/reboot

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -f "$HOME/.zsh_history "] ; then
+    rm "$HOME/.zsh_history"
+fi
+
 if [ -d "$HOME/.recycle-bin" ] ; then
     doas rm -r "$HOME/.recycle-bin/" && doas shutdown -h now
 else
