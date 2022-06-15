cf1917298a688a261593e337bed22c4c4662f292
tomit4 <mosssap@gmail.com>
2022-06-15 14:23:55 -0700

    figured out how to clear history on startup of zsh

--- scripts/reboot.sh
+++ scripts/reboot.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ -f "$HOME/.zsh_history " ] ; then
-    rm "$HOME/.zsh_history"
-fi
-
 if [ -d "$HOME/.recycle-bin" ] ; then
     doas rm -r "$HOME/.recycle-bin/" && doas reboot
 else
