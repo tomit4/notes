72bc26c6228c16abdd4c1e0e7ad5a785bd65e2c1
z3rOR0ne <z3rOR0ne@protonmail.com>
2022-12-17 16:02:40 -0800

    :memo: Updated reboot/shutdown.sh scripts

--- scripts/reboot.sh
+++ scripts/reboot.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-last_line_first_word=$(awk '{w=$1} END{print w}' $HOME/.zshrc)
-
-if [[ $last_line_first_word == "export" ]] ; then
-    sed -i '$ d' $HOME/.zshrc
-fi
-
 if [ -d "$HOME/.recycle-bin" ] ; then
     doas rm -r "$HOME/.recycle-bin/" && doas reboot
 else
