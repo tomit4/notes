ab7b3a65ed60911f0d1c83e2fc4c3b076380bff4
z3rOR0ne <z3rOR0ne@protonmail.com>
2022-12-18 18:05:39 -0800

    :boom: Reformatted neovim config

---
+++ the_primeagen_nvim_setup/after/plugin/harpoon.lua
@@ -0,0 +1,10 @@
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<S-j>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<S-k>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<S-h>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<S-l>", function() ui.nav_file(4) end)
