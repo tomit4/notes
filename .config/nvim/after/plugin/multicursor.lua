4dac72aa1b9b19be7827ef465f631bb8ab2a0d65
z3rOR0ne <z3rOR0ne@protonmail.com>
2022-12-18 18:06:59 -0800

    :sparkles: Completely revamped neovim config

---
+++ .config/nvim/after/plugin/multicursor.lua
@@ -0,0 +1,1 @@
+vim.opt.cursorcolumn = true
---
+++ .config/nvim/after/plugin/nerdcommenter.lua
@@ -0,0 +1,6 @@
+-- Create Default Mappings for NerdCommenter
+vim.g.NERDCreateDefaultMappings = 1
+-- Add spaces after NerdCommenter delimiters by default
+vim.g.NERDSpaceDelims = 1
+-- Enable Comments with Italics
+-- vim.cmd[[highlight Comment cterm=italic gui=italic]]
