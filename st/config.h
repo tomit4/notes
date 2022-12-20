6beb8a200f0e55af90eb9fd9fc7de346c6bf3cd9
z3rOR0ne <z3rOR0ne@protonmail.com>
2022-12-20 01:51:41 -0800

    :sparkles: Resized default terminal size in float

--- st/config.h
+++ st/config.h
@@ -151,8 +151,10 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+// static unsigned int cols = 80;
+static unsigned int cols = 140;
+// static unsigned int rows = 24;
+static unsigned int rows = 36;
 
 /*
  * Default colour and shape of the mouse cursor
