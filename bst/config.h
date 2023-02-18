57da6d24e9535206c201d8e895f904a8d8a956f1
z3rOR0ne <z3rOR0ne@protonmail.com>
2023-02-17 23:03:39 -0800

    :memo: Small change to bst config

--- bst/config.h
+++ bst/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=18.5:antialias=true:autohint=true";
+static char *font = "Source Code Pro:pixelsize=18:antialias=true:autohint=true";
 // static char *font = "mono:pixelsize=18.5:antialias=true:autohint=true";
 static int borderpx = 2;
 
