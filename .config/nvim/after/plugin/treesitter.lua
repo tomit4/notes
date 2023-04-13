cc28c790f67e1af8a61db83c2de2e04ac838e906
z3rOR0ne <z3rOR0ne@protonmail.com>
2023-04-13 00:18:03 -0700

    :bug: Reverted back treesitter due to nvim breakage

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,57 +1,60 @@
-vim.treesitter.query.set("javascript", "injections", "")
-vim.treesitter.query.set("typescript", "injections", "")
-vim.treesitter.query.set("tsx", "injections", "")
-vim.treesitter.query.set("lua", "injections", "")
+-- vim.treesitter.query.set("javascript", "injections", "")
+-- vim.treesitter.query.set("typescript", "injections", "")
+-- vim.treesitter.query.set("tsx", "injections", "")
+-- vim.treesitter.query.set("lua", "injections", "")
+vim.treesitter.set_query("javascript", "injections", "")
+vim.treesitter.set_query("typescript", "injections", "")
+vim.treesitter.set_query("tsx", "injections", "")
+vim.treesitter.set_query("lua", "injections", "")
 require("nvim-treesitter.configs").setup({
-	-- A list of parser names, or "all"
-	ensure_installed = {
-		"bash",
-		"c",
-		"c_sharp",
-		"cmake",
-		"cpp",
-		"css",
-		"dockerfile",
-		"go",
-		"html",
-		"http",
-		"java",
-		"javascript",
-		"json",
-		"json5",
-		"jsonc",
-		"lua",
-		"make",
-		"perl",
-		"php",
-		"pug",
-		"python",
-		"ruby",
-		"toml",
-		"tsx",
-		"typescript",
-		"rust",
-		"vim",
-		"vue",
-		"wgsl",
-		"yaml",
-	},
+        -- A list of parser names, or "all"
+        ensure_installed = {
+                "bash",
+                "c",
+                "c_sharp",
+                "cmake",
+                "cpp",
+                "css",
+                "dockerfile",
+                "go",
+                "html",
+                "http",
+                "java",
+                "javascript",
+                "json",
+                "json5",
+                "jsonc",
+                "lua",
+                "make",
+                "perl",
+                "php",
+                "pug",
+                "python",
+                "ruby",
+                "toml",
+                "tsx",
+                "typescript",
+                "rust",
+                "vim",
+                "vue",
+                "yaml",
+        },
 
-	-- Install parsers synchronously (only applied to `ensure_installed`)
-	sync_install = false,
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
 
 	-- Automatically install missing parsers when entering buffer
 	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	auto_install = true,
+        auto_install = true,
 
-	highlight = {
+        highlight = {
 		-- `false` will disable the whole extension
-		enable = true,
+                enable = true,
 
 		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 		-- Using this option may slow down your editor, and you may see some duplicate highlights.
 		-- Instead of true it can also be a list of languages
-		additional_vim_regex_highlighting = false,
-	},
+                additional_vim_regex_highlighting = false,
+        },
 })
