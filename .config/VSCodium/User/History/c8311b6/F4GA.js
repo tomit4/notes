423aa812e90cea74d63fb8c5acfb01bcfbaa9e1f
z3rOR0ne <z3rOR0ne@protonmail.com>
2023-08-12 04:16:49 -0700

    :memo: Installed alacritty and codium

---
+++ .config/VSCodium/User/History/c8311b6/F4GA.js
@@ -0,0 +1,32 @@
+import { db } from '../utils/db.js'
+
+class Authenticator {
+    constructor() {
+        this.currentUser = null
+    }
+    async sendAuthEmail(answered) {
+        return await db.post('/user/sendemail/', answered)
+    }
+    async verifyAuthSession(hashedToken) {
+        return await db.get(`/user/verify/${hashedToken}`)
+    }
+    async getAccessToken(req) {
+        return await db.post('/user/getaccess', req, true)
+    }
+    async validateSession(hashedAccessToken) {
+        return await db.post('/user/validatesession', hashedAccessToken, true)
+    }
+    async authenticateLoginCredentials(credentials) {
+        return await db.post('/user/login', credentials)
+    }
+    async removeSession(hashedAccessToken) {
+        return await db.post('/user/removesession', hashedAccessToken, true)
+    }
+}
+const authenticator = new Authenticator()
+
+export { authenticator, Authenticator }
+const something = 
+() => {
+    console.log('wow :=>')
+}
