15261e78dfbcd48f8ab7b689b7138b79f32c0ddd
z3rOR0ne <z3rOR0ne@protonmail.com>
2023-11-28 05:25:53 -0800

    :wrench: Nvim update breaks dap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
 require("neovim")
 require("impatient")
-require("config.dap.javascript")
+-- require("config.dap.javascript")
 -- very recent error: https://github.com/rcarriga/nvim-dap-ui/issues/233
 -- require("dapui").setup()
