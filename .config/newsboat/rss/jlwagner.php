c82f2c24673d8f667c15a80e4df9a4e6f77e2cd4
z3rOR0ne <z3rOR0ne@protonmail.com>
2024-02-26 07:05:54 -0800

    :wrench: Sub to jlwager blog

---
+++ .config/newsboat/rss/jlwagner.php
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0">
+  <channel>
+    <title>Jeremy Wagner</title>
+    <description>The home page of technical writer, author, and occasional speaker Jeremy Wagner.</description>
+    <link>https://jlwagner.net/</link>
+            <item>
+          <title>Optimize long tasks</title>
+          <link>https://web.dev/optimize-long-tasks/</link>
+          <description>You've been told to &quot;don't block the main thread&quot; and &quot;break up your long tasks&quot;, but what does it mean to do those things?</description>
+          <pubDate>Mon, 30 Sep 2024 20:22:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>2022 Web Almanac: JavaScript</title>
+          <link>https://almanac.httparchive.org/en/2022/javascript</link>
+          <description>JavaScript chapter of the 2022 Web Almanac covering the usage of JavaScript on the web, libraries and frameworks, compression, web components, and source maps.</description>
+          <pubDate>Wed, 25 Sep 2024 20:22:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>Cycles</title>
+          <link>https://jlwagner.net/blog/cycles/</link>
+          <description>It isn’t “bullying” or “boys being boys”. It’s intimidation and violence — and it will consume us all if we don’t intervene.</description>
+          <pubDate>Wed, 29 May 2024 20:22:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>Don’t fight the browser preload scanner</title>
+          <link>https://web.dev/preload-scanner/</link>
+          <description>Find out what the browser preload scanner is, how it helps performance, and how you can stay out of its way.</description>
+          <pubDate>Mon, 13 May 2024 20:22:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>Interaction to Next Paint (INP)</title>
+          <link>https://web.dev/inp/</link>
+          <description>Interaction to Next Paint (INP) is an experimental field metric that assesses responsiveness. INP logs the latency of all interactions throughout the entire page lifecycle.</description>
+          <pubDate>Mon, 6 May 2024 20:22:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>I went and wrote a book</title>
+          <link>https://jlwagner.net/blog/i-went-and-wrote-a-book/</link>
+          <description>I wrote Responsible JavaScript for A Book Apart. I hope you’ll consider buying it, and if you do, I hope you like it.</description>
+          <pubDate>Sun, 27 Oct 2024 20:21:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>Responsible JavaScript</title>
+          <link>https://abookapart.com/products/responsible-javascript</link>
+          <description>Learn how to use JavaScript responsibly and create resilient experiences that are more accessible and inclusive.</description>
+          <pubDate>Sat, 26 Oct 2024 20:21:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>Time to First Byte (TTFB)</title>
+          <link>https://web.dev/ttfb/</link>
+          <description>Time to First Byte (TTFB) is a foundational metric for measuring connection setup time and web server responsiveness in both the lab and the field. It helps identify when a web server is too slow to respond to requests. In the case of navigation requests — that is, requests for an HTML document — it precedes every other meaningful loading performance metric.</description>
+          <pubDate>Sat, 26 Oct 2024 20:21:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>Assessing Loading Performance in the Field with Navigation Timing and Resource Timing</title>
+          <link>https://web.dev/navigation-and-resource-timing/</link>
+          <description>Learn the basics of using the Navigation and Resource Timing APIs to assess loading performance in the field.</description>
+          <pubDate>Tue, 8 Oct 2024 20:21:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>Now THAT’S What I Call Service Worker!</title>
+          <link>https://alistapart.com/article/now-thats-what-i-call-service-worker/</link>
+          <description>If you’re looking to achieve the single-page app level performance without the overhead (and boot time) of a huge JavaScript library or having to completely rewrite your website in a new technology, Jeremy Wagner shares a clever approach combining Service Worker and streamed web page partials you’re sure to love.</description>
+          <pubDate>Mon, 18 Mar 2024 20:21:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>radEventListener: A Tale of Client-side Framework Performance</title>
+          <link>https://css-tricks.com/radeventlistener-a-tale-of-client-side-framework-performance/</link>
+          <description>React has its place when complex state management is required. In my experience, however, it doesn’t belong in most scenarios. When even a bit of React is a problem on devices slow and fast alike, using it is a choice to exclude those on low-end hardware.</description>
+          <pubDate>Mon, 19 Aug 2024 20:20:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>Innovation Can’t Keep the Web Fast</title>
+          <link>https://css-tricks.com/innovation-cant-keep-the-web-fast/</link>
+          <description>Every so often, the fruits of innovation bear fruit in the form of improvements to the foundational layers of the web. Unfortunately, web developers are locked in an arms race between these foundational improvements, and ever-increasing page sizes.</description>
+          <pubDate>Wed, 31 Jan 2024 20:20:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>Defining productivity</title>
+          <link>https://jlwagner.net/blog/defining-productivity/</link>
+          <description>In which I ask what the true meaning of productivity is when it comes to web development.</description>
+          <pubDate>Sun, 31 Mar 2024 20:19:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>Responsible JavaScript: Part I</title>
+          <link>https://alistapart.com/article/responsible-javascript-part-1</link>
+          <description>The web is drowning in a sea of JavaScript, awash with unnecessary bloat, inaccessible cruft, and unsustainable patterns. Jeremy Wagner plots a course to navigate the JavaScript Sea responsibly by building the right things the right way and using the web platform the way it was meant to be used.</description>
+          <pubDate>Thu, 28 Mar 2024 20:19:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>Make it boring</title>
+          <link>https://jlwagner.net/blog/make-it-boring/</link>
+          <description>In which I make the case that the web we build should be more boring to better benefit those who use what we make.</description>
+          <pubDate>Fri, 9 Feb 2024 20:19:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>The WebP Manual</title>
+          <link>https://www.smashingmagazine.com/ebooks/the-webp-manual/</link>
+          <description>In this ebook, you’ll learn all about WebP: what it’s capable of, how it performs, how to convert images to the format in a variety of ways, and most importantly, how to use it.</description>
+          <pubDate>Wed, 24 Jul 2024 20:18:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>Help Your Users `Save-Data`</title>
+          <link>https://css-tricks.com/help-users-save-data/</link>
+          <description>When enabled, Chrome’s Data Saver mode sends a `Save-Data` request header you can use to help users conserve data. How you will help your users `Save-Data`?</description>
+          <pubDate>Wed, 2 Oct 2024 20:17:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>Next Generation Server Compression With Brotli</title>
+          <link>https://www.smashingmagazine.com/2016/10/next-generation-server-compression-with-brotli/</link>
+          <description>Chances are pretty good that you’ve worked with, or at least understand the concept of, server compression. By compressing website assets on the server prior to transferring them to the browser, we’ve been able to achieve substantial performance gains. In this article, learn how you can squeeze even more performance out of your website with Brotli.</description>
+          <pubDate>Sat, 5 Oct 2024 20:16:00 America/Los_Angeles</pubDate>
+        </item>
+                <item>
+          <title>`font-display` for the Masses </title>
+          <link>https://css-tricks.com/font-display-masses/</link>
+          <description>Default rendering behavior for web fonts in browsers can be fraught with peril, and sometimes can even cause accessibility problems. Learn how you can regain control of web font rendering with ease using the CSS `font-display` property!</description>
+          <pubDate>Mon, 19 Aug 2024 20:16:00 America/Los_Angeles</pubDate>
+        </item>
+          </channel>
+</rss>
