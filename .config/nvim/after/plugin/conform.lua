155c696af8d0fa03073a379e88d2f3167b7b904d
z3rOR0ne <z3rOR0ne@protonmail.com>
2024-08-12 15:09:40 -0700

    :wrench: Just ensuring rustfmt is for sure

--- .config/nvim/after/plugin/conform.lua
+++ .config/nvim/after/plugin/conform.lua
@@ -7,6 +7,7 @@ require("conform").setup({
 		typescript = { "prettierd", "prettier", stop_after_first = true },
 		html = { "prettierd", "prettier", stop_after_first = true },
 		sql = { "sql_formatter" },
+		rust = { "rustfmt" },
 		sh = { "shfmt" },
 		json = { "jq" },
 		toml = { "taplo" },
