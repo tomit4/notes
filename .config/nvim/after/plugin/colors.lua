ee26b9289e89a94afcc94eb8cfcd80284cb03616
z3rOR0ne <z3rOR0ne@protonmail.com>
2024-12-29 03:59:35 -0800

    :wrench: More adjustments editor

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -5,9 +5,9 @@ function ColorMyPencils(color)
 	-- vim.api.nvim_set_hl(0, "@attribute", { fg = "#B48EAD", italic = true }) -- For Tree-sitter
 	-- vim.api.nvim_set_hl(0, "@decorator", { fg = "#B48EAD", italic = true }) -- Some setups may use this
 
-	-- vim.api.nvim_set_hl(0, "TabLine", { bg = "none", fg = "#81A1C1" }) -- Inactive tabs
-	-- vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none", fg = "#D8DEE9" }) -- Active tab
-	-- vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" }) -- Background area behind tabs
+	vim.api.nvim_set_hl(0, "TabLine", { bg = "none", fg = "#81A1C1" }) -- Inactive tabs
+	vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none", fg = "#D8DEE9" }) -- Active tab
+	vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" }) -- Background area behind tabs
 
 	-- vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#BF616A", bg = "none" }) -- Nord red
 	-- vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { fg = "#EBCB8B", bg = "none" }) -- Nord yellow
